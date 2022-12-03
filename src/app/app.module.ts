647e2fd997e452a077c523275d8b1aeb58ad3c48
Teng Wei Zhun <90449947+WeiZhunTeng@users.noreply.github.com>
2022-12-03 22:08:25 +0800

    Add employee component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderNameIdComponent } from './header-name-id/header-name-id.component';
 import { SideMenuComponent } from './side-menu/side-menu.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { SideMenuComponent } from './side-menu/side-menu.component';
     NavbarComponent,
     HeaderNameIdComponent,
     SideMenuComponent,
+    EmployeeComponent,
   ],
   imports: [
     BrowserModule,
