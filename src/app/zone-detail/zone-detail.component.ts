894025f7a1c554e08102970e315eca2f2a1a8d25
YjLau1118 <93077477+YjLau1118@users.noreply.github.com>
2022-12-05 13:04:29 +0800

    Zone Detail

--- src/app/zone-detail/zone-detail.component.ts
+++ src/app/zone-detail/zone-detail.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import {ConfirmationService, ConfirmEventType, MessageService} from 'primeng/api';
+import {Message} from 'primeng/api';
+import { PrimeNGConfig } from 'primeng/api';
+import { NgxQrcodeElementTypes, NgxQrcodeErrorCorrectionLevels } from '@techiediaries/ngx-qrcode';
 
 @Component({
   selector: 'app-zone-detail',
   templateUrl: './zone-detail.component.html',
-  styleUrls: ['./zone-detail.component.css']
+  styleUrls: ['./zone-detail.component.css'],
+  providers: [ConfirmationService,MessageService]
 })
 export class ZoneDetailComponent implements OnInit {
 
-  constructor() { }
+  elementType = NgxQrcodeElementTypes.URL;
+  correctionLevel = NgxQrcodeErrorCorrectionLevels.HIGH;
+  value = 'hahaha';
+  
+  panelOpenState: boolean = false;
+  constructor(private confirmationService: ConfirmationService, private messageService: MessageService, private primengConfig: PrimeNGConfig) {}
+
+  confirm1() {
+    this.confirmationService.confirm({
+        message: 'Are you sure that you want to proceed?',
+        header: 'Confirmation',
+        icon: 'pi pi-exclamation-triangle',
+        accept: () => {
+          this.messageService.add({severity:'success', summary:'Confirmed', detail:'You have accepted'});
+        },
+        reject: () => {
+          this.messageService.add({severity:'success', summary:'Rejected', detail:'You have rejected'});
+      }
+    });
+}
 
   ngOnInit(): void {
+    this.primengConfig.ripple = true;
   }
+  displayBasic: boolean;
 
+  showBasicDialog() {
+    this.displayBasic = true;
+}
 }
