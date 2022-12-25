773a7743d4844356ffd08e645572afd8e2cbfc34
DavidChoo84 <davidchoo84@gmail.com>
2022-12-25 22:23:07 +0800

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
-const routes: Routes = [
-  // {path:'',redirectTo:'zone_crud',pathMatch:'full'},
-  // {path: 'zone_crud', loadChildren: ()=>import('./zonecrud/crud.module').then(m=>m.CRUDModule)}
+const routes: Routes = [  
+  {path: 'zone_crud', loadChildren: ()=>import('./zonecrud/crud.module').then(m=>m.CRUDModule)}
 ];
 
 @NgModule({
