c6e41a1de8692c0f562e7860212ec23bfa08129b
DavidChoo84 <davidchoo84@gmail.com>
2022-12-29 15:50:26 +0800

    add firebase
    use to push notification to mobile apps

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'buloke-47e9d',
+    appId: '1:202932134311:web:35a302a87cf2eba9725682',
+    storageBucket: 'buloke-47e9d.appspot.com',
+    apiKey: 'AIzaSyC7IQeef1TmDeVaYGj6U3HF7j_lTYDXzjs',
+    authDomain: 'buloke-47e9d.firebaseapp.com',
+    messagingSenderId: '202932134311',
+    measurementId: 'G-P6BNVLWDD9',
+  },
   production: true,
   API_EndPoint: 'https://bulokeworks.com/php-local/webphp/' 
 };
