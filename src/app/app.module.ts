2e83f79b1a9978e7684683d0ec611cf39e200051
Teng Wei Zhun <90449947+WeiZhunTeng@users.noreply.github.com>
2023-02-03 12:48:43 +0800

    Add multiselect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -54,6 +54,7 @@ import { SiteComponent } from './site/site.component';
 
 import { EmployeeReportModal } from './modal/employee-report/employee-report.modal';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 
 @NgModule({
   declarations: [
@@ -106,7 +107,8 @@ ReactiveFormsModule,
     MatPaginatorModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideMessaging(() => getMessaging()),
-    NgbModule
+    NgbModule,
+    NgMultiSelectDropDownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
